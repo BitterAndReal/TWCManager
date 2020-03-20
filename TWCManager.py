fb36862c676e3f8564f0aa4c8d3159854baaa5d2
BitterAndReal <34364368+BitterAndReal@users.noreply.github.com>
2020-03-20 20:59:27 +0100

    Update TWCManager.py

--- TWCManager.py
+++ TWCManager.py
@@ -1410,7 +1410,7 @@ def check_utility_fuse_current():
 
 
     if(len(mains) >= 15):
-        backgroundTasksLock.acquire()
+#        backgroundTasksLock.acquire()
         
         # We're only interested in the three current measurements
         # put the L1, L2 & L3 Amps in a list
@@ -1419,58 +1419,17 @@ def check_utility_fuse_current():
         MainsAmpsPhases[1] = mains[8]
         MainsAmpsPhases[2] = mains[13]
 
-        del mains[:] # delete the mains list after we used the values we need
 
-        # Define how many List are taken to calculate an average
-        # A small current spike should not trigger the main fuse.
-        # 1,1 x In for one hour // 1,5 x In for 10 min // 2 x In for 1 min // 3 x In for 10s // 10 x In for 0.1s
-        MaxMainsListLength = 10
-
-        # create maxMainsList list if it does not exist (don't know if this is necessary?)
-        if not(maxMainsList):
-            maxMainsList = []
-
-        # find phase with highest current which is the limit for all phases and insert at beginning of List list
-        maxMainsList.append(max(MainsAmpsPhases))
-
-        # remove oldest value in list (slice list to MaxMainsListLength size)
-        maxMainsList = maxMainsList[:MaxMainsListLength]
-
-        # get max value in the List
-        maxMainsAmps = max(maxMainsList)
+        maxMainsAmps = max(MainsAmpsPhases)
         # maxMainsAmps = sum(maxMainsList) / len(maxMainsList)
 
-        # lower current for at least 15 minuts
-        if(maxMainsAmps > lastMaxMainsAmps):
-            lastMaxMainsAmps = maxMainsAmps
-            maxMainsAmpsChangeTime = now
-        elif(now - maxMainsAmpsChangeTime < (60 * 15)):
-            maxMainsAmps = lastMaxMainsAmps
-        else:
-            lastMaxMainsAmps = maxMainsAmps
-
-
-
-
-        # avgMainsAmps can be used instead of solar generation api
-
-        #AvgMainsListLength = 60
-
-        # create avgMainsList list if it does not exist (don't know if this is necessary?)
-        if not(avgMainsList):
-            avgMainsList = []
+  
 
         # calculate average of all phases and insert at beginning of list
-        avgMainsList.append(sum(MainsAmpsPhases) / len(MainsAmpsPhases))
+        avgMainsAmps = sum(MainsAmpsPhases) / len(MainsAmpsPhases)
 
         # remove oldest value in list (slice list to AvgMainsListLength size)
         #avgMainsList = avgMainsList[:AvgMainsListLength]
-
-        # calculate average of the list and change avgMainsAmps if it is not changed in the last 5 minuts
-        if(now - avgMainsAmpsChangeTime < (60 * 5)):
-            avgMainsAmps = sum(avgMainsList) / len(avgMainsList) 
-            del avgMainsList[:]
-            avgMainsAmpsChangeTime = now
 
 
 
@@ -1479,7 +1438,6 @@ def check_utility_fuse_current():
               " Amps L1 " + str(MainsAmpsPhases[0]) +
               " Amps L2 " + str(MainsAmpsPhases[1]) +
               " Amps L3 " + str(MainsAmpsPhases[2]) +
-              " last mains List " + str(maxMainsList[0]) +
               " max mains Amps " + str(maxMainsAmps) +
               " avg mains Amps " + str(avgMainsAmps))
 
@@ -1493,12 +1451,8 @@ def check_utility_fuse_current():
         print(time_now() +
         " ERROR: Can't connect to utility mains current sensor! ")
 
-        del maxMainsList[:]
-        del avgMainsList[:]
-        del mains[:]
 
-        avgMainsAmps = 0
-        leftOverAmpsForAllTWCs = 0
+
 
 
 
